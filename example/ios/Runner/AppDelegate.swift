e759e3eeecac337451d009c1f7e5063ce7d43022
Mark Abramenko <abramenko@surfstudio.ru>
2022-01-26 15:52:25 +0300

    Add `RPEntry.initialize`

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,14 +8,10 @@ import RaxelPulse
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      // Documentation: https://docs.damoov.com/docs/-download-the-sdk-and-install-it-in-your-environment#setting-up-the-permissions-wizard
-      RPPermissionsWizard.returnInstance().launch(finish: { _ in
-          RPEntry.initialize(withRequestingPermissions: true)
-          let token = NSString(string: "Please, enter your Token")
-          RPEntry.instance().virtualDeviceToken = token
-          let options = launchOptions ?? [:]
-          RPEntry.application(application, didFinishLaunchingWithOptions: options)
-      })
+      RPEntry.initialize(withRequestingPermissions: false)
+      
+      let options = launchOptions ?? [:]
+      RPEntry.application(application, didFinishLaunchingWithOptions: options)
       
       GeneratedPluginRegistrant.register(with: self)
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
